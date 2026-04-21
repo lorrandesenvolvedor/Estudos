8c95504b2486f1bba5b956f85dadd74379888255
Lorran C. S. <lorrandesenvolvedor@gmail.com>
2026-04-21 16:17:00 -0300

    Create append.py

---
+++ pythonEstudos/append.py
@@ -0,0 +1,14 @@
+#no método append nao preciso mais criar varias variaveis pois apenas uma ja e suficiente.
+
+#exemplo;
+
+frutas = [] #lista vazia
+
+#agora são as variáveis que iremos ussr aoenas uma para varios dados.
+frutas.append("maça")
+frutas.append("banana")
+print(frutas)
+#no fim usamos print + variavel para simplesmente imprimir o conteúdo.
+
+
+#OBS, APPEND E UMA LISTA QUE CONTÉM VARIOS DADOS DE UMA SO VARIÁVEL.
